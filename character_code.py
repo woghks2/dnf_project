ad4f06c1e488d56e9a9e0155d158c2bd75ed3cc6
woghks2 <tbxkdls19@gmail.com>
2024-05-07 17:04:47 +0900

    feat : 캐릭터 코드를 불러오는 파일을 추가했습니다.

--- character_code.py
+++ character_code.py
@@ -5,14 +5,11 @@ from config import API_KEY,BASE_PATH
 import pandas as pd
 import os
 import glob
-
 loader = DNFAPI(API_KEY)
 
-base_path = BASE_PATH
-folder_path = os.path.join(base_path,'crawling_data','data_20240220')
-csv_files = glob.glob(os.path.join(folder_path, '*.csv'))
 # %%
 def get_character_code(sv_eng, cha_name):
+    # 캐릭터 코드 가져오기 / 오류 나는 경우는 None
     try:
         cha_code = loader.get_character(sv_eng, cha_name)
         return cha_code['characterId'][0]
@@ -20,29 +17,35 @@ def get_character_code(sv_eng, cha_name):
         print(f"Error getting character code for {cha_name}: {e}")
         return None
 
-def run(csv_files):
+def run(file_path):
+    
+    df = pd.read_csv(file_path)
+    total = len(df)
+    cha_codes = [None]*total
 
-    for csv_file in csv_files:
-        print(csv_file)
+    # 병렬 처리
+    with ThreadPoolExecutor(max_workers=8) as executor:
+        futures = {executor.submit(get_character_code,row['sv_eng'],row['cha_name']):
+                    i for i, row in df.iterrows()}
+        # 맞는 인덱스에 전달받은 캐릭터 코드 입력
+        for future in as_completed(futures):
+            index = futures[future]
+            try:
+                cha_code = future.result()
+                cha_codes[index] = cha_code
+                print(index)
+            except Exception as e:
+                print(f"Error in future: {e}")
 
-        df = pd.read_csv(csv_file)
-        total = len(df)
-        cha_codes = [None]*total
+    df['cha_code'] = cha_codes
+    # 결과 데이터프레임 저장
+    df.to_csv(f'{file_path}', index=False, encoding = 'utf-8-sig')
+    print(f'{file_path} saved')
 
-        with ThreadPoolExecutor(max_workers=8) as executor:
-            futures = {executor.submit(get_character_code,row['sv_eng'],row['cha_name']):
-                       i for i, row in df.iterrows()}
-            for future in as_completed(futures):
-                index = futures[future]
-                try:
-                    cha_code = future.result()
-                    cha_codes[index] = cha_code
-                except Exception as e:
-                    print(f"Error in future: {e}")
-
-        df['cha_code'] = cha_codes
-        # 결과 데이터프레임 저장
-        df.to_csv(f"{csv_file.split('.')[0]}_with_codes.csv", index=False, encoding = 'utf-8')
-        print(f'{csv_file} 완료')
-
-run(csv_files)
+# %%
+path = os.path.join(BASE_PATH,'data\crawling_data\data_20240503\*.csv')
+file_names = glob.glob(path)
+# %%
+for file_name in file_names:
+    print(file_name)
+    run(file_name)
